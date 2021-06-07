0c6ebc223f489d9bc2f0ac05e4c82b04f36c9cf9
Shreehari Nair <47605111+shreeharin@users.noreply.github.com>
2021-06-07 10:49:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+(function () {
+
+var names = ["Jafer", "John", "Sam", "james", "Bucky", "barnes", "johnson"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
