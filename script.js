e362c3c15c1f5a3439726e3affe806ad9ba55039
Shreehari Nair <47605111+shreeharin@users.noreply.github.com>
2021-06-07 11:02:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Jafer", "John", "Sam", "james", "Bucky", "barnes", "johnson"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
